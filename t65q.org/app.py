e47e770734c1655d026866766d9b398cb94d0a80
Aaron Helton <mariusagricola@gmail.com>
2018-08-16 14:28:39 -0400

    route skeleton

--- t65q.org/app.py
+++ t65q.org/app.py
@@ -1,1 +1,28 @@
 from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+
+    return render_template('index.html')
+
+@app.route('/news', methods=['GET'])
+def news():
+
+    return "foo"
+
+@app.route('/events', methods=['GET'])
+def events():
+
+    return "foo"
+
+@app.route('/about', methods=['GET'])
+def about():
+
+    return "foo"
+
+@app.route('/contact', methods=['GET'])
+def contact():
+
+    return "foo"
