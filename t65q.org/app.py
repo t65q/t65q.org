e1d19cb62c4c9467b958cfe2a4152ecce8e77193
Aaron Helton <mariusagricola@gmail.com>
2018-08-17 14:48:28 -0400

    build out structure and functionality

--- t65q.org/app.py
+++ t65q.org/app.py
@@ -1,28 +1,45 @@
 from flask import Flask, render_template
+from ics import Calendar
+from ics.timeline import Timeline
+from urllib.request import urlopen
+import arrow
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    # To do: async this?
+    
 
     return render_template('index.html')
 
-@app.route('/news', methods=['GET'])
-def news():
-
-    return "foo"
-
 @app.route('/events', methods=['GET'])
 def events():
+    # To do: async this?
+    ical_url = "https://www.scoutbook.com/ics/62638.E7F02.ics"
+    calendar = Calendar(urlopen(ical_url).read().decode('iso-8859-1'))
+    timeline = Timeline(calendar).start_after(arrow.utcnow())
+    events = []
+    for event in timeline:
+        event_data = {
+            "name":event.name, 
+            "begin_human":event.begin.humanize(),
+            "description":event.description.replace('HTTP','http'), 
+            "location":event.location, 
+            "begin":event.begin.format('dddd, MMM D, YYYY h:mm A'), 
+            "end":event.end.format('dddd, MMM D, YYYY h:mm A'),
+            "url":event.url
+        }
+        events.append(event_data)
 
-    return "foo"
+    return render_template('events.html', context={"events": events})
 
 @app.route('/about', methods=['GET'])
 def about():
 
-    return "foo"
+    return render_template('about.html')
 
 @app.route('/contact', methods=['GET'])
 def contact():
 
-    return "foo"
+    return render_template('contact.html')
