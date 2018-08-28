9ae28f22f6380ecf91f3d17a8bf8fb9a3e7a88b9
Aaron Helton <mariusagricola@gmail.com>
2018-08-28 12:45:13 -0400

    import request from flask

--- t65q/app.py
+++ t65q/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from ics import Calendar
 from ics.timeline import Timeline
 from urllib.request import urlopen
